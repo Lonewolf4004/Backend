207259aa2f3c93171117e35d1da731e54cf605f8
ShaikhZaib <shaikhzaib04@gmail.com>
2025-08-12 15:58:54 +0530

    add

---
+++ SQLClass/index.js
@@ -0,0 +1,47 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.use("views", Path.join(__dirname, "/views"));
+
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    database : 'delta_app',
+    password : 'Z@ib0406'
+});
+
+let getRandomUser = () => {
+    return [
+        faker.string.uuid(),
+        faker.internet.username(), //  before version 9.1.0, use userName()
+        faker.internet.email(),
+        faker.internet.password()
+    ];
+}
+
+
+app.get('/', (req, res)=>{
+    let q = `SELECT COUNT(*) FROM user`;
+    connection.query(q, (err, result) => {
+        if(err) {
+            console.log(err);
+            res.send("Some error in database");
+            return;
+        }
+        console.log(result[0]['COUNT(*)']);
+        res.send("success");
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Port is listening at port : ${port}`);
+});
+
+
+    
