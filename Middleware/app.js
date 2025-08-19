a4d9e149f93fdfb5a3591c0fe317b822dc030b4b
ShaikhZaib <shaikhzaib04@gmail.com>
2025-08-19 19:40:58 +0530

    Update app.js

--- Middleware/app.js
+++ Middleware/app.js
@@ -2,6 +2,25 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+
+app.use((req, res, next)=>{
+    console.log("Hi, i am 1st middleware.");
+    next();
+    console.log("This is after next()");
+});
+app.use((req, res, next)=>{
+    console.log("Hi, i am 2nd middleware.");
+    next();
+});
+
+app.get("/", (req, res)=>{
+    res.send("Hi, i am root.");
+});
+
+app.get("/random", (req, res)=>{
+    res.send("This is a random page");
+});
+
 app.listen(port, ()=>{
     console.log(`App is listening on port : ${port}`);
 });
