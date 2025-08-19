ebced8675e5e7b1de19f10d5b1608a2d22b7ca6e
ShaikhZaib <shaikhzaib04@gmail.com>
2025-08-19 20:18:44 +0530

    Update app.js

--- Middleware/app.js
+++ Middleware/app.js
@@ -3,15 +3,21 @@ const app = express();
 const port = 8080;
 
 
-app.use((req, res, next)=>{
-    console.log("Hi, i am 1st middleware.");
+// app.use((req, res, next)=>{
+//     console.log("Hi, i am 1st middleware.");
+//     next();
+// });
+// app.use((req, res, next)=>{
+//     console.log("Hi, i am 2nd middleware.");
+//     next();
+// });
+
+app.use("/random", (req, res, next)=>{
+    console.log("I am only for random");
     next();
-    console.log("This is after next()");
 });
-app.use((req, res, next)=>{
-    console.log("Hi, i am 2nd middleware.");
-    next();
-});
+
+
 
 app.get("/", (req, res)=>{
     res.send("Hi, i am root.");
@@ -19,6 +25,17 @@ app.get("/", (req, res)=>{
 
 app.get("/random", (req, res)=>{
     res.send("This is a random page");
+});
+
+
+app.use((req, res, next) =>{
+    req.time= new Date(Date.now()).toString();
+    console.log(req.method, req.hostname, req.path, req.time);
+    next();
+});
+
+app.use((req, res, next) =>{
+    res.send("Page not found");
 });
 
 app.listen(port, ()=>{
